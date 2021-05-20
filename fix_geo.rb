c3eed02616d490b75d628147cc0240ab2e918d40
Maura Carbone <maura_carbone@harvard.edu>
2021-05-20 11:19:58 -0400

    Fix some of the math

--- fix_geo.rb
+++ fix_geo.rb
@@ -19,42 +19,62 @@ def fix_bbox(bbox_string, layer, bad_file)
     if list_nums[0] == list_nums[1]
         bad_file.append(layer)
         size=list_nums[0].split("").size-1
-        if list_nums[0].to_f>=0
-            if size ==1
+        if list_nums[0].to_f>0 && list_nums[0].to_f<180
+            if !list_nums[1].include?"."
                 list_nums[1]=list_nums[1] + ".001"
-            elsif size < 3
-                list_nums[1]=list_nums[1] + "001"
-            else
-                list_nums[1]=list_nums[1]+"1"
+            else size < 3
+                if
+                    list_nums[1]=list_nums[1] + "001"
+                else
+                    list_nums[1]=list_nums[1]+"1"
+                end
             end
+        elsif list_nums[0].to_f==180
+            list_nums[0]="179.999"
+        elsif list_nums[0].to_f==0
+            list_nums[1]="0.001"
+        elsif list_nums[0].to_f==-180
+            list_nums[1]="-179.999"
         else
-            if size == 1
+            if !list_nums[0].include?"."
                 list_nums[0]=list_nums[0] + ".001"
-            elsif size < 3
-                list_nums[0]=list_nums[0] + "001"
-            else
-                list_nums[0]=list_nums[0]+"1"
+            else 
+                if size < 3
+                    list_nums[0]=list_nums[0] + "001"
+                else
+                    list_nums[0]=list_nums[0]+"1"
+                end
             end
         end
     end
     if list_nums[2] == list_nums[3]
         bad_file.append(layer)
         size=list_nums[2].split("").size-1
-        if list_nums[2].to_f>=0
-            if size ==1
+        if list_nums[2].to_f>0 && list_nums[2].to_f<90
+            if !list_nums[2].include?"."
                 list_nums[2]=list_nums[2] + ".001"
-            elsif size < 3
-                list_nums[2]=list_nums[2] + "001"
-            else
-                list_nums[2]=list_nums[2]+"1"
+            else 
+                if size < 3
+                    list_nums[2]=list_nums[2] + "001"
+                else
+                    list_nums[2]=list_nums[2]+"1"
+                end
             end
+        elsif list_nums[2].to_f==90
+            list_nums[3]="89.999"
+        elsif list_nums[2].to_f==0
+            list_nums[2]="0.001"
+        elsif list_nums[2].to_f==-90
+            list_nums[2]="-89.999"
         else
-            if size == 1
+            if !list_nums[3].include?"."
                 list_nums[3]=list_nums[3] + ".001"
-            elsif size < 3
-                list_nums[3]=list_nums[3] + "001"
-            else
-                list_nums[3]=list_nums[3]+"1"
+            else 
+                if size < 3
+                    list_nums[3]=list_nums[3] + "001"
+                else
+                    list_nums[3]=list_nums[3]+"1"
+                end
             end
         end
     end
@@ -62,7 +82,7 @@ def fix_bbox(bbox_string, layer, bad_file)
     return bbox_string
 end
 
-file_list=Dir['/path/to/files/**/*.json']
+file_list=Dir['/path/to/data/**/*.json']
 
 for file in file_list
     bad_stuff=[]
@@ -72,10 +92,10 @@ for file in file_list
         if bad_stuff.include?(json_file['layer_id_s'])
 	        puts 'writing new file'
             puts json_file['layer_id_s']
-            File.open(file, 'w') { |f| f.write(json_file.to_json) }
+            File.open(file, 'w') { |f| f.write(JSON.pretty_generate(json_file)) }
         end
     end
-    changes = File.open('/path/to/files/fixed.txt', 'a')
+    changes = File.open('/path/to/data/fixed.txt', 'a')
     bad_stuff=bad_stuff.uniq
     for item in bad_stuff
         changes.write(item.to_s + "\n")
